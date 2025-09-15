cd4cc2258b34f2dd049a9aec5d4ac714f207a670
Pavel Eliseev <paaleliseev@edu.hse.ru>
2025-09-15 20:44:26 +0300

    Up char-sum

--- tasks/01-basic-io/char-sum/solution.cpp
+++ tasks/01-basic-io/char-sum/solution.cpp
@@ -1,6 +1,47 @@
-#include <cctype>  // std::isdigit
 #include <unistd.h>
 
 int main() {
-    // TODO: your code here.
+    constexpr size_t BUFSZ = 4096;
+    char buf[BUFSZ];
+
+    unsigned long long sum = 0;
+    while (true) {
+        ssize_t n = read(STDIN_FILENO, buf, BUFSZ);
+        if (n <= 0) {
+            break;
+        }
+        for (ssize_t i = 0; i < n; ++i) {
+            unsigned char ch = static_cast<unsigned char>(buf[i]);
+            if (ch >= '0' && ch <= '9') {
+                sum += static_cast<unsigned long long>(ch - '0');
+            }
+        }
+    }
+
+    char out[64];
+    int len = 0;
+    if (sum == 0) {
+        out[len++] = '0';
+    } else {
+        char tmp[64];
+        int t = 0;
+        while (sum > 0) {
+            tmp[t++] = static_cast<char>('0' + (sum % 10));
+            sum /= 10;
+        }
+        while (t > 0) {
+            out[len++] = tmp[--t];
+        }
+    }
+    out[len++] = '\n';
+
+    int written = 0;
+    while (written < len) {
+        ssize_t w = write(STDOUT_FILENO, out + written, len - written);
+        if (w <= 0) {
+            break;
+        }
+        written += static_cast<int>(w);
+    }
+    return 0;
 }
